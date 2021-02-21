1ee1587c82358ec2b4c757dbbcf17812c0dc76e7
Renata Simaitiene <simaitiene.renata@gmail.com>
2021-02-21 16:45:43 +0200

    fixed navigation style for mobile

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -1,4 +1,9 @@
 const navToggle = document.querySelector('.js-hamburger');
+const menu = document.querySelector('.navigation__list');
+
 navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('hamburger--opened');
+  const opened = navToggle.classList.contains('hamburger--opened');
+  navToggle.setAttribute('aria-expanded', opened);
+  menu.classList.toggle('navigation__list--opened');
 });
